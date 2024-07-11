dbe73b6fac1152ff4fc5ed6a5f25a307c1f9eb93
Bhanupal22600 <bhanu22600@iiitd.ac.in>
2024-07-11 14:16:49 +0530

    new

--- String/prac.cpp
+++ String/prac.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	
-	    int x;
-	    cin>>x;
-	    
-	   if(x==2 || x==4){
-	       return 0;
-	   } 
-	   
-	   //check
-	   int k=128000;
+int cal(int x) {
+    if(x == 2 || x == 4) {
+        return 0;
+    } 
+
+    // Find the largest power of 2 less than or equal to x
+    int k=131072;
 	   while(k>x){
 	   k=k/2;
        }
        int ans=0;
        for(int i=k;i<k*2;i+=2){
-           
-           if(i==x){
-               cout<< ans;
-               break;
-           }
-            else{
-                ans+=4;
-            }
-       }
-       cout<< ans;
+        if(i == x) {
+            break;
+        } else {
+            ans += 4;
+        }
     }
+    
+    return ans;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        int x;
+        cin >> x;
+        cout << cal(x) << endl; // Print the result of cal(x)
+    }
+
+    return 0;
+}
 
