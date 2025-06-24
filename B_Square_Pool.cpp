845861e097fed405dc1afdcafab8a96dcd2e29ea
Bhanupal22600 <bhanu22600@iiitd.ac.in>
2025-06-25 00:57:59 +0530

    n

--- B_Square_Pool.cpp
+++ B_Square_Pool.cpp
@@ -4,9 +4,28 @@ using namespace std;
 void calc() {
     int n,s;
     cin>>n>>s;
-    while(n--){
-        cin>>
+    int ans=0;
+    int t=n;
+    while(t--){
+        int dx,dy,x,y;
+        cin>>dx>>dy>>x>>y;
+        if(s%2==0){
+            if(x==y && dx*dy==1){
+                ans++;
+            }
+            else if(x==s/2 && y==s/2) ans++;
+            else if(x+y==s && dx*dy==-1) ans++;
+        }
+        else{
+            if(x==y && dx*dy==1){
+                ans++;
+            }
+            else if(x+y==s && dx*dy==-1) ans++;
+        }
     }
+    cout<<ans<<endl;
+    return;
+
 }
 
 int main() {
